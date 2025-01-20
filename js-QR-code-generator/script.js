292232e03b5723545e8f68564417595020ab9014
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-01-20 18:10:14 +0200

    Removing empty spaces from the input

--- js-QR-code-generator/script.js
+++ js-QR-code-generator/script.js
@@ -7,12 +7,13 @@ let button = document.querySelector(".button");
 button.addEventListener("click", generateQR);
 
 function generateQR(){
-  if (input.value.length > 0){
+  if (input.value.trim().length > 0){
     let qrApi = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
 
     qrImage.src = qrApi;
     displayQR.classList.add("show-img")
   }else {
+    input.value = '';
     input.classList.add("error");
     setTimeout(() => {
       input.classList.remove("error");
