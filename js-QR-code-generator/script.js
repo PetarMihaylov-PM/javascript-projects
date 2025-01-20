7392fedbd5e97bfea92b915229929cd7c5d32d02
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-01-20 18:04:09 +0200

    qr-code-generator upload

---
+++ js-QR-code-generator/script.js
@@ -0,0 +1,22 @@
+
+let displayQR = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let input = document.getElementById("input");
+let button = document.querySelector(".button");
+
+button.addEventListener("click", generateQR);
+
+function generateQR(){
+  if (input.value.length > 0){
+    let qrApi = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+
+    qrImage.src = qrApi;
+    displayQR.classList.add("show-img")
+  }else {
+    input.classList.add("error");
+    setTimeout(() => {
+      input.classList.remove("error");
+    }, 1000)
+  }
+}
+ 
