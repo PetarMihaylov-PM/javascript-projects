926d8d0212d4c56172edbf1b885601f3ac7d9adb
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-01-24 16:59:15 +0200

    Updating the music player

--- js-music-player/script.js
+++ js-music-player/script.js
@@ -1,2 +1,25 @@
 
  
+let prograssBar = document.getElementById('progress');
+let song = document.getElementById('song');
+let playPauseButton = document.getElementById('playPause');
+
+playPauseButton.addEventListener('click', stopStart);
+
+song.onloadedmetadata = function(){
+  prograssBar.max = song.duration;
+  prograssBar.value = song.currentTime;
+}
+
+function stopStart(){
+  if(playPauseButton.classList.contains("fa-pause")){
+    song.pause();
+    playPauseButton.classList.remove("fa-pause"); 
+    playPauseButton.classList.add("fa-play"); 
+  }
+  else{
+    song.play();
+    playPauseButton.classList.remove("fa-play"); 
+    playPauseButton.classList.add("fa-pause"); 
+  }
+}
