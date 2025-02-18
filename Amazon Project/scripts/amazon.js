0ee3fff69e9caf5d2c5bc0d2c178f7ed0b08f9f6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-02-18 21:28:08 +0200

    Create a function for calculating the cart quantity, and import it to amazon.js and checkout.js

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,6 +1,8 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
+updateCartQuantity();
 
 let productsHTML = '';
 
@@ -61,13 +63,9 @@ products.forEach(product => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-      cart.forEach(cartItem => {
-        cartQuantity += cartItem.quantity;
-      })
-
-      document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
+  
+  document.querySelector('.js-cart-quantity')
+  .innerHTML = calculateCartQuantity();
 }
 
 function displayAddedText(productId, clearId){
--- Amazon Project/scripts/checkout.js
+++ Amazon Project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -113,11 +113,7 @@ document.querySelectorAll(".js-delete-link")
 
 
   function updateCartQuantity () {
-    let cartItemsQuantity = 0;
-    cart.forEach(cartItem => {
-      cartItemsQuantity += cartItem.quantity; 
-    });
 
-    document.querySelector(".js-checkout-cart-quantity").innerHTML = cartItemsQuantity;
+    document.querySelector(".js-checkout-cart-quantity").innerHTML = calculateCartQuantity();
   };
  
