0d833dccbff31e3356686ccee6e019c748d7eb19
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-05 20:51:18 +0200

    mock localStorage.setItem

--- Amazon Project/tests-jasmine/data/cartTest.js
+++ Amazon Project/tests-jasmine/data/cartTest.js
@@ -6,6 +6,9 @@ describe('Test suit: addToCart', () => {
   });
 
   it('adds an new product to the cart', () => {
+
+    spyOn(localStorage, 'setItem');
+
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
@@ -14,5 +17,6 @@ describe('Test suit: addToCart', () => {
     
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 });
