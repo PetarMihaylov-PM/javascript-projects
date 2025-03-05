8eef0a7f302d239b178a2649d402bb7026632131
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-05 18:03:35 +0200

    testing addToCart() with jasmine

--- Amazon Project/tests-jasmine/data/cartTest.js
+++ Amazon Project/tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe('Test suit: addToCart', () => {
   it('adds an existing product to the cart', () => {
@@ -6,6 +6,13 @@ describe('Test suit: addToCart', () => {
   });
 
   it('adds an new product to the cart', () => {
-    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-  })
-})
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]);
+    });
+    
+    loadFromStorage();
+    
+    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart.length).toEqual(1);
+  });
+});
