26d0ecf996a6e92e03251841c3b461e4e7b0e5de
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-05 17:15:07 +0200

    testing with Jasmine

---
+++ Amazon Project/tests-jasmine/data/cartTest.js
@@ -0,0 +1,11 @@
+import { addToCart } from "../../data/cart.js";
+
+describe('Test suit: addToCart', () => {
+  it('adds an existing product to the cart', () => {
+
+  });
+
+  it('adds an new product to the cart', () => {
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+  })
+})
