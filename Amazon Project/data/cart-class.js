37b07eafcf1872db284d7bb73eeb80b7ae703450
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-06 16:18:12 +0200

    Update cart-class.js

--- Amazon Project/data/cart-class.js
+++ Amazon Project/data/cart-class.js
@@ -1,6 +1,5 @@
 class Cart{
-  cartItems = undefined;
-  localStorageKey = undefined;
+  cartItems;
 
   constructor(localStorageKey){
     this.localStorageKey = localStorageKey;
@@ -106,8 +105,6 @@ class Cart{
 
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
-
-
 
 console.log(cart);
 console.log(businessCart);
