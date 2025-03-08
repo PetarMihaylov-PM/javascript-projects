afa1d53edf0298c4526e1a9734e03175401888cd
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-08 22:07:31 +0200

    load products from XHR to fetch

--- Amazon Project/data/products.js
+++ Amazon Project/data/products.js
@@ -58,6 +58,22 @@ class Clothing extends (Product){
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json()
+  }).then((produtsData) => {
+    products = produtsData.map((productDetails) => {
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });    
+  });
+  return promise;
+}
+
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -68,7 +84,7 @@ export function loadProducts(fun) {
       }
       return new Product(productDetails);
     });
-    
+
     fun();
   });
 
