bfd8b99ca0816d92f252e9a56ee514bc0c8a1866
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-09 22:38:44 +0200

    make Place order button interactive

--- Amazon Project/scripts/checkout/paymentSummary.js
+++ Amazon Project/scripts/checkout/paymentSummary.js
@@ -50,7 +50,7 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
   `
@@ -58,4 +58,18 @@ export function renderPaymentSummary() {
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHtml;
 
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      console.log(order);
+    });
 }
