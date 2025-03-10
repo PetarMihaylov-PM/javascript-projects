3e17bd3df99ff504f077256e226cce5c71b33cae
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-10 19:36:24 +0200

    generating the HTML for the orders page

--- Amazon Project/scripts/orders.js
+++ Amazon Project/scripts/orders.js
@@ -1,79 +1,106 @@
 import { orders } from "../data/orders.js";
-import { getProduct, products, loadProductsFetch } from "../data/products.js";
+import { getProduct, loadProductsFetch } from "../data/products.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { formatCurrency } from "./utils/money.js";
 
 async function loadPage() {
   try{
     await loadProductsFetch();
-    const value = await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-      });
-    });
 
   } catch(error) {
     console.log('Unexpected error. Please try again later.');
   }
 
-  let html = '';
-
-  console.log(orders);
+  
+  let ordersHtml = ``;
+  console.log(orders)
 
   orders.forEach(order => {
+   
+    let productsHtml = ``;
 
     const orderId = order.id;
-    const orderDate = order.orderTime; // have to modify
-    console.log(orderId , orderDate)
+    const orderDate = getDate(order.orderTime);
+    const totalCostCents = orders.totalCostCents;
 
     const orderProducts = order.products;
 
     orderProducts.forEach(product => {
       
       const productId = product.productId;
-      const deliveryDateProduct = product.estimatedDeliveryTime.split('T')[0];
-      const formatedDeliveryDate = dayjs(deliveryDateProduct).format('MMMM D');
-      console.log(formatedDeliveryDate)
-      
+      const [deliveryDateProduct] = product.estimatedDeliveryTime.split('T');
       
       const quantity = product.quantity;
       
       let matchingProduct = getProduct(productId);
       
 
-      html += `
-      <div class="product-image-container">
-        <img src="${matchingProduct.image}">
-      </div>
+      productsHtml += `
+      <div class="order-details-grid">
+        <div class="product-image-container">
+          <img src="${matchingProduct.image}">
+        </div>
 
-      <div class="product-details">
-        <div class="product-name">
-          ${matchingProduct.name}
-        </div>
-        <div class="product-delivery-date">
-          Arriving on: ${formatedDeliveryDate}
-        </div>
-        <div class="product-quantity">
-          Quantity: ${quantity}
-        </div>
-        <button class="buy-again-button button-primary js-buy-again-button">
-          <img class="buy-again-icon" src="images/icons/buy-again.png">
-          <span class="buy-again-message">Buy it again</span>
-        </button>
-      </div>
-
-      <div class="product-actions">
-        <a href="tracking.html?orderId=${orderId}&productId=${productId}">
-          <button class="track-package-button button-secondary">
-            Track package
+        <div class="product-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-delivery-date">
+            Arriving on: ${getDate(deliveryDateProduct)}
+          </div>
+          <div class="product-quantity">
+            Quantity: ${quantity}
+          </div>
+          <button class="buy-again-button button-primary js-buy-again-button">
+            <img class="buy-again-icon" src="images/icons/buy-again.png">
+            <span class="buy-again-message">Buy it again</span>
           </button>
-        </a>
+        </div>
+
+        <div class="product-actions">
+          <a href="tracking.html?orderId=${orderId}&productId=${productId}">
+            <button class="track-package-button button-secondary">
+              Track package
+            </button>
+          </a>
+        </div>
       </div>
       `
     });
 
+    ordersHtml += `
+    <div class="order-container">
+      <div class="order-header">
+        <div class="order-header-left-section">
+          <div class="order-date">
+            <div class="order-header-label">Order Placed:</div>
+            <div>${orderDate}</div>
+          </div>
+          <div class="order-total">
+            <div class="order-header-label">Total:</div>
+            <div>${formatCurrency(totalCostCents)}</div>
+          </div>
+        </div>
+
+        <div class="order-header-right-section">
+          <div class="order-header-label">Order ID:</div>
+          <div>${orderId}</div>
+        </div>
+      </div>
+
+      ${productsHtml}
+
+    </div>
+    `
   });
 }
 loadPage();
 
+
+function getDate(estimatedDate) {
+  const [dateOnly] = estimatedDate.split('T');
+  const modifiedDate = dayjs(dateOnly).format('MMMM D');
+  return modifiedDate;
+}
 
 
